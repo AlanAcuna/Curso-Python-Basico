eb56207aa8d0a8ad1934516372ff1fb0dd64d7b0
AlanAcuna <alanmra96@outlook.es>
2021-08-02 23:37:40 -0500

    primer commit

---
+++ Primer script/hola_mundo.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  2 23:31:35 2021
+
+@author: Alan
+"""
+
+#Esto es un comentario
+
+"""
+------------------------------
+BIENVENIDO AL CURSO BÁSICO DE PYTHON
+------------------------------
+"""
+
+print ("¡Hola Mundo cruel y despiadado!");
+print ("Bienvenido");
